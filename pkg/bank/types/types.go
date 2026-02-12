8a65fd3d7ab482858d41a25ae4fd498aa3a4e7c9
Shodmon <shodvaly@gmail.com>
2026-02-12 19:31:30 +0500

    committsss

--- pkg/bank/types/types.go
+++ pkg/bank/types/types.go
@@ -1,7 +1,34 @@
 package types
 
-type Card struct{
-	ID int
-	Balance int
-	
+// Money представляет собой денежную сумму в минимальных единицах (центы, копейки, дирамы и т.д.)
+type Money int64
+
+// Currency представляет код валюты
+type Currency string
+
+// Коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+	EUR Currency = "EUR"
+)
+
+// PAN представляет номер карты
+type PAN string
+
+// Card представляет информацию о платежной карте
+type Card struct {
+	ID         int
+	Pan        PAN
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+type Payment struct {
+	ID     int
+	Amount Money
 }
